3901495f521b99894d34ec6b151fe112f775fffe
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2023-06-09 18:16:44 +0530

    Time: 7 ms (33.48%), Space: 9.6 MB (61.57%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int l = 0;
-        int h = nums.size() - 1;
-
-        while (l <= h) {
-            int mid = (l + h) / 2;
-            
-            if (nums[mid] == target) {
+        //if target is present in the array
+        int l=0;
+        int h=nums.size()-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(nums[mid]==target)
                 return mid;
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
-            }
+            else if(nums[mid]<target)
+                l=mid+1;
+            else
+                h=mid-1;
         }
-
-        l = 0;
-        h = nums.size() - 1;
-
-        while (l <= h) {
-            int mid = (l + h) / 2;
-
-            if (nums[mid] < target && (mid + 1 >= nums.size() || nums[mid + 1] > target)) {
-                return mid + 1;
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
-            }
+        //if target is not present in the array
+         l=0;
+         h=nums.size()-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            //mid+1 cannot be checked if it is not present so check if 
+            if(nums[mid]<target && (mid+1 >= nums.size() || nums[mid+1]>target ))
+                return mid+1;
+            else if(nums[mid]<target)
+                l=mid+1;
+            else
+                h=mid-1;
         }
-
-        return 0; // or any other appropriate default value
+        //if target element is the smallest element to be added
+        return 0;
     }
 };
