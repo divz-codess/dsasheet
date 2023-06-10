1991026f928ff741bf5a8f79da83ae4d29125f9b
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2023-06-10 16:30:42 +0530

    Time: 22 ms (84.57%), Space: 32.3 MB (9.16%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-     int ls=0, s=0;
-    for(int i=0;i<nums.size();i++){
-        s+=nums[i];
-    }
-        for(int i=0;i<nums.size();i++){
-            s-=nums[i];
-            if(ls==s) return i;
-            ls+=nums[i];
+        int n=nums.size();
+        if(n==1) return 0;
+        vector<int> l(n);
+        l[0]=nums[0];
+        vector<int> r(n);
+        r[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            l[i]=nums[i]+l[i-1];
+            r[n-i-1]=nums[n-i-1]+r[n-i];
+        }
+        for(int i=0;i<n;i++){
+            if(l[i]==r[i])
+                return i;
         }
         return -1;
-        //TC= O(n)
-        //SC= O(1)
     }
 };
