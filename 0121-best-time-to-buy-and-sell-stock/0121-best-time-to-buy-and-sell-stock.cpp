c75002e690bc75847905a19275aad7cf459ef36e
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2023-08-26 14:31:55 +0530

    Time: 96 ms (75.58%), Space: 93.4 MB (37.46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    /*
+    ALGO:
+    1. Take a minval variable and initialize it to INT_MAX.
+    2. Now keep finding the minimum price at which we can sell
+    3. Also keep finding profit if we sell it today
+    4. And store maximum profit thst can be generated at all times by storing it in overallpr variable
+    */
     int maxProfit(vector<int>& prices) {
-        int minival=INT_MAX;
-        int p=0;
-        int op=0;
+        int leastvalue=INT_MAX;
+        int pr=0;
+        int ovpr=0;
         for(int i=0;i<prices.size();i++){
-            if(prices[i]<minival){
-                minival=prices[i];
+            if(prices[i]<leastvalue){
+                leastvalue=prices[i];
             }
-            p=prices[i]-minival;
-            if(p>op){
-                op=p;
+            pr=prices[i]-leastvalue;
+            if(ovpr<pr){
+                ovpr=pr;
             }
         }
-        return op;
+        return ovpr;
     }
 };
