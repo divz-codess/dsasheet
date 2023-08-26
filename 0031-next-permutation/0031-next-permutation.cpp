63830d24b9c8c0b7bf11bd022947632ff8194f5d
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2023-08-26 12:14:24 +0530

    Time: 4 ms (60.09%), Space: 12.2 MB (22.90%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        /*
+        ALGO:
+        1. Take a variable ind to stor the index of dip or the point where we see that the next no. is smaller when travelling from back
+        2. Traverse through the vector to find the dip point
+        3. If we donot find any index then the array is in descending order already so reverse it as there is no permutation greater than it
+        4. Find the number traversing from back which is greater than the number at ind and it will be the 1st no. to be found as we donot have any dip points as checked already.
+        5. When found swap it and break;
+        6. Now just reverse the array from nums.begin()+ind+1 to end.
+        */
         int ind=-1;
         int n=nums.size();
         for(int i=n-2;i>=0;i--){
