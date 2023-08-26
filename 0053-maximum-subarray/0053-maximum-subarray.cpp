63350140105a28c10f97d038116b316e4e26d566
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2023-08-26 13:37:50 +0530

    Time: 90 ms (62.18%), Space: 67.7 MB (36.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+/*
+KADANE'S ALGO: 
+1. Traverse through the array and keep finding currsum
+2. We find the currsum by finding maximum of either only using the current no. or by adding the sum of the previous numbers too.
+3. If our value is incrementing add it else no
+4. Also we keep updating value of max sum if we find any value larger than max sum, this is done instead of storing maxsum for each number in the array and later on finding the largest of them.
+
+*/
     int maxSubArray(vector<int>& nums) {
         int maxsum=INT_MIN;
         int currsum=0;
