1871c24d3c20ed3264cd7dfe65417fe7dca84de8
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2023-08-31 14:06:52 +0530

    Time: 11 ms (52.99%), Space: 7.5 MB (18.80%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int> m1;
+        map<char,int> m2;
+        for(auto i: s){
+            m1[i]++;
+        }
+        for(auto i: t){
+            m2[i]++;
+        }
+        if(m1==m2) return true;
+        return false;
+    }
+};
