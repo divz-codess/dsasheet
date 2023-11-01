8934716472a8071e491e38b48a7f2252aac8268c
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2023-11-01 20:55:31 +0530

    Time: 111 ms (31.78%), Space: 72 MB (17.20%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i: nums){
+            mp[i]++;
+        }
+        for(auto i: mp){
+            if(i.second>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
