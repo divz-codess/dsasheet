b9f2da2716801c37a830592f546a70ab10e0ae9a
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2023-11-02 14:32:13 +0530

    Time: 3 ms (45.24%), Space: 6.6 MB (49.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> ss;
-        for(auto &i: s){
-            if(i=='(' || i=='{' || i=='[' ){
+        for(char& i: s){
+            if(i=='('||i=='{'||i=='['){
                 ss.push(i);
             }
             else{
                 if(ss.empty()) return false;
                 char c=ss.top();
                 ss.pop();
-                if(c=='(' && i==')'||c=='{' && i=='}'|| c=='[' && i==']'){
+                if(c=='('&&i==')'||c=='{'&&i=='}'||c=='['&&i==']'){
                     continue;
                 }
                 else return false;
             }
         }
         if(ss.empty()) return true;
-        else return false;
+        return false;
     }
 };
