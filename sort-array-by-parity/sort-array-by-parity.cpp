2c22a10025a90f820b52c2a32036df89997ab292
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2024-01-21 13:01:54 +0530

    Time: 9 ms, Memory: 18.7 MB - LeetHub

--- sort-array-by-parity/sort-array-by-parity.cpp
+++ sort-array-by-parity/sort-array-by-parity.cpp
@@ -5,13 +5,13 @@ public:
         int k=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]%2==0){
-                nums[k++]=nums[i];
+                swap(nums[k++],nums[i]);
             }
-            else odd.push_back(nums[i]);
+            // else odd.push_back(nums[i]);
         }
-        for(int i=0;i<odd.size();i++){
-            nums[k++]=odd[i];
-        }
+        // for(int i=0;i<odd.size();i++){
+        //     nums[k++]=odd[i];
+        // }
         return nums;
     }
 };
