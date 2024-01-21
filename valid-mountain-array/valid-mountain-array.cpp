2dc299d6f8cb8ef1cb2c2899a29fb9e3e1961331
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2024-01-21 11:24:04 +0530

    Time: 19 ms, Memory: 24.8 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int p=1;
+        if(arr.size()<3) return false;
+        while(p<arr.size()){
+            if(arr[p]>arr[p-1]) p++;
+            else break;
+        }
+        if(p==arr.size() || p==1) return false;
+        while(p<arr.size()){
+            if(arr[p] < arr[p-1]) p++;
+            else break;
+        }
+        if(p==arr.size()) return true;
+        else return false;
+    }
+};
