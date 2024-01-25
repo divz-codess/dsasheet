74c937ce3961da439f18aadfc24e263c3a22e49e
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2024-01-25 20:00:03 +0530

    Time: 3 ms (86.21%), Space: 18.5 MB (5.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,15 +11,33 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(INT_MIN);
-        ListNode* ans=&dummy;
-        
-        while(list1 && list2){
-            (list1->val<=list2->val)?(ans->next=list1,list1=list1->next):(ans->next=list2,list2=list2->next);
-            ans=ans->next;
+        ListNode *head=new ListNode(-1);
+        ListNode *head2=head;
+        while(list1 !=NULL && list2!=NULL){
+            if(list1->val<=list2->val){
+                ListNode *nn=new ListNode(list1->val);
+                head->next=nn;
+                head=head->next;
+                list1=list1->next;
+            }
+            else{
+                ListNode *nn=new ListNode(list2->val);
+                head->next=nn;
+                head=head->next;
+                list2=list2->next;
+            }
+        }
+        if(list1==NULL && list2==NULL){
+            return head2->next;
+        }
+        else if(list1==NULL){
+            head->next=list2;
             
         }
-        ans->next=list1?(list1):(list2);
-        return dummy.next;
+        else if(list2==NULL){
+            head->next=list1;
+            
+        }
+        return head2->next;
     }
 };
