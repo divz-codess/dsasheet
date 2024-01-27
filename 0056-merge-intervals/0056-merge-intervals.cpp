79ca10ce5349627794631694e6fba29f9e0390c6
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2024-01-27 13:58:15 +0530

    Time: 23 ms (92.89%), Space: 22.7 MB (11.00%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> merging(vector<vector<int>>& intervals){
-        vector<vector<int>> ans;
-        int siz=intervals.size();
-        int i;
-        for( i=0;i<intervals.size()-1;i++){
-            if((intervals[i+1][0]>=intervals[i][0] && intervals[i+1][0]<=intervals[i][1]) || (intervals[i][0]>=intervals[i+1][0] && intervals[i][0]<=intervals[i+1][1])){
-                vector<int> a;
-                a.push_back(min(intervals[i][0],intervals[i+1][0]));
-                a.push_back(max(intervals[i][1],intervals[i+1][1]));
-                ans.push_back(a);
-                i++;
-                siz--;
-            }
-            else 
-                ans.push_back(intervals[i]);
-        }
-        
-        if(siz!=ans.size()) 
-            ans.push_back(intervals[i]);
-        return ans;
-    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        if(intervals.size()<=0) return intervals;
         sort(intervals.begin(),intervals.end());
-        vector<vector<int>> ans1=merging(intervals);
-        vector<vector<int>> ans2;
-        while(true){
-            ans1=merging(ans1);
-            ans2=merging(ans1);
-            if(ans1==ans2) break;
+        
+        ans.push_back(intervals[0]);
+        
+        int j=0;
+        for(int i=1;i<intervals.size();i++){
+            if(ans[j][1] >= intervals[i][0]){
+                ans[j][1]=max(ans[j][1],intervals[i][1]);
+            }
+            else{
+                j++;
+                ans.push_back(intervals[i]);
+            }
         }
-        return ans1;
+        return ans;
     }
 };
