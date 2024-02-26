097b7992a4188d282adb8c223f013ded705480e4
Divyangi Raghav <88512639+divz-codess@users.noreply.github.com>
2024-02-26 11:43:46 +0530

    Time: 70 ms (85.67%), Space: 49.2 MB (86.23%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,15 +5,16 @@ public:
         int cnt=1;
         int n=nums.size();
         sort(nums.begin(), nums.end());
+        
         for(int i=0;i<n;i++){
-            if(i< n-1 && nums[i]==nums[i+1]){
+            if(i<n-1 && nums[i]==nums[i+1]){
                 continue;
             }
             else if(i< n-1 && nums[i]+1==nums[i+1]){
                 cnt++;
             }
             else{
-                maxi=max(maxi,cnt);
+                maxi=max(maxi, cnt);
                 cnt=1;
             }
         }
